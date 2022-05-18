2890263945010b4d89b7741f08efe1f2b807682d
isabela2b <Isabel@a2bsolutiongroup.com>
2022-05-18 15:40:07 +0800

    added document splitting

--- api.py
+++ api.py
@@ -1,22 +1,9 @@
 from flask import Flask, request, jsonify
+from functions import parse_classify, update_model, model_classify, allowed_file
 
-import pytesseract
-import cv2 as cv
-import pandas as pd
 import numpy as np
-import io
-import csv
-import time
-
-import docx
-from pdf2image import convert_from_bytes
+import cv2 as cv
 from PIL import Image
-from sklearn import preprocessing
-
-import tensorflow as tf
-from tensorflow import keras
-from keras.models import load_model
-
 import traceback
 import logging
 #from logging.handlers import RotatingFileHandler
@@ -27,130 +14,7 @@ app = Flask(__name__)
 #logging.basicConfig(filename='demo.log', level=logging.DEBUG)
 #handler = RotatingFileHandler(os.path.join(app.root_path, 'logs', 'error_log.log'), maxBytes=102400, backupCount=10)
 
-
-app = Flask(__name__)
-
-poppler_path = r"C:\Users\fora2\Documents\poppler-21.03.0\Library\bin" #for windows
-
-model = load_model('models/classify_256.h5')
-doc_type = []
-
-# Allowed extension you can set your own
-ALLOWED_EXTENSIONS = set(['pdf', 'png', 'jpg', 'jpeg', 'docx', 'xlsx', 'xls'])
-THRESHOLD = 90
-
-with open('doc_type.csv', newline='') as inputfile:
-    for row in csv.reader(inputfile):
-        doc_type.append(row[0])
-
-doc_type.sort()
-
-
-def allowed_file(filename):
-    return '.' in filename and file_ext(filename) in ALLOWED_EXTENSIONS
-
-def file_ext(filename):
-	return filename.rsplit('.', 1)[1].lower()
-
-def img_to_string(image):
-	"""Takes an image then parses text into a string"""
-	image_string = pytesseract.image_to_string(image, lang='eng', config='--psm 1 --oem 3')
-	return image_string
-
-def grayscale(image):
-	return cv.cvtColor(image, cv.COLOR_BGR2GRAY)
-
-def img_preprocess(image):
-	image = cv.resize(image, (256,256))
-	cv.imwrite('convert.jpg', image)
-	im=cv.imread('convert.jpg')
-
-	im = im/255
-	im = np.expand_dims(im, axis=0)
-	return im
-
-def model_classify(image):
-	gray = grayscale(image)
-	image = img_preprocess(gray)
-	holistic_pred=model.predict(image)
-	sort_index=np.argsort(holistic_pred)[::-1]
-	df=pd.DataFrame({'Document_Type':doc_type,
-                         'Percentage':holistic_pred[0]})
-	df=df.sort_values('Percentage',ascending=False)
-	labels=df['Document_Type']
-	classification = df.iloc[0]['Document_Type']
-	percentage =  df.iloc[0]['Percentage']*100
-	df['Percentage'] = df['Percentage']*100
-	rank = df.iloc[0:5].reset_index(drop=True).set_index('Document_Type')['Percentage'].to_dict()
-
-	if percentage < THRESHOLD: # and percentage != "other":
-		try:
-			image_string = img_to_string(gray)
-			key_classification = key_classify(image_string)
-		except Exception as e:
-			f = open("log.txt", "a")
-			f.write(str(e))
-			f.write(traceback.format_exc())
-			f.close()
-		return key_classification, percentage, rank
-	else:
-		return classification, percentage, rank
-
-def key_classify(string):
-	string = string.lower()
-	if "packing declaration" in string:
-		print("pkd check done")
-		return "pkd"
-	elif "packing list" in string:
-		print("pkl check done")
-		return "pkl"
-	elif "bill of lading" in string:
-		print("hbl check done")
-		return "hbl"
-	elif "invoice" in string:
-		print("civ check done: ")
-		return "civ"
-	else:
-		return "other" 
-
-
-def parse_classify(file):
-	"""
-	Receives file 
-	Note that this only reads the first sheet or page of the document,
-	since that's where the header/title is usually located. 
-
-	Returns classification of document based on keyword
-	"""
-
-	ext = file_ext(file.filename)
-	string = ""
-	accuracy = 0
-	rank = {}
-
-	if ext == "pdf":
-		images = convert_from_bytes(file.read())
-		classification, accuracy, rank = model_classify(np.asarray(images[0]))
-
-
-	elif ext in ["jpg", "jpeg", "png"]:
-		pil_image = Image.open(file)
-		opencvImage = cv.cvtColor(np.array(pil_image), cv.COLOR_RGB2BGR)
-		classification, accuracy, rank = model_classify(opencvImage)
-	elif ext == "docx":
-		doc = docx.Document(file)
-		fullText = []
-		for para in doc.paragraphs:
-			fullText.append(para.text)
-			string = '\n'.join(fullText)
-		classification = key_classify(string)
-
-	elif ext in ["xls", "xlsx"]:
-		sheet = pd.read_excel(file, sheet_name=[0])
-		string = str(sheet)
-		classification = key_classify(string)
-
-	return classification, accuracy, rank
+#poppler_path = r"C:\Users\fora2\Documents\poppler-21.03.0\Library\bin" #for windows
 
 @app.route('/classify' , methods=['POST'])
 def classify():
@@ -175,9 +39,8 @@ def classify():
 		instance = {}
 		for file in files:
 			if file and allowed_file(file.filename):
-				file_type, accuracy, rank = parse_classify(file)
-				data["files"].append({'file name': file.filename, 'file size in bytes': file.seek(0,2) ,'type': file_type, 'accuracy':accuracy, 'rank': rank})
-
+				file_type, prediction = parse_classify(file)
+				data["files"].append({'file name': file.filename, 'file size in bytes': file.seek(0,2), 'file type': file_type, 'prediction': prediction})		
 	return jsonify(data)
 
 @app.route('/learn' , methods=['POST'])
@@ -222,23 +85,6 @@ def learn():
 			else:
 				return "File format not supported."
 
-def update_model(X,y):
-	try:
-		gray = grayscale(X)
-		X = img_preprocess(gray)
-		le = preprocessing.LabelBinarizer()
-		le.fit(doc_type)
-		y = le.transform([y])
-		model.fit(X,y)
-		model.save('models/classify_256.h5', overwrite=True)
-		return "Success!"
-	except Exception as e:
-		#f = open("log.txt", "a")
-		#f.write(str(e))
-		#f.write(traceback.format_exc())
-		#f.close()
-		return e
-	#return "Success!"
 
 @app.route('/check' , methods=['POST'])
 def check():
---
+++ functions.py
@@ -0,0 +1,187 @@
+import pytesseract
+import cv2 as cv
+import pandas as pd
+import numpy as np
+import io, csv, time, os
+
+import docx
+from pdf2image import convert_from_bytes
+from PIL import Image
+from PyPDF2 import PdfFileWriter, PdfFileReader, PdfFileMerger
+from sklearn import preprocessing
+
+import tensorflow as tf
+from tensorflow import keras
+from keras.models import load_model
+
+model = load_model('models/classify_512.h5')
+data_folder = "merge/"
+doc_type = []
+
+# Allowed extension you can set your own
+ALLOWED_EXTENSIONS = set(['pdf', 'png', 'jpg', 'jpeg', 'docx', 'xlsx', 'xls'])
+THRESHOLD = 90
+
+with open('doc_type.csv', newline='') as inputfile:
+    for row in csv.reader(inputfile):
+        doc_type.append(row[0])
+
+doc_type.sort()
+
+def allowed_file(filename):
+    return '.' in filename and file_ext(filename) in ALLOWED_EXTENSIONS
+
+def file_ext(filename):
+	return filename.rsplit('.', 1)[1].lower()
+
+def file_name(filename):
+    return filename.rsplit('.', 1)[0]
+
+def img_to_string(image):
+	"""Takes an image then parses text into a string"""
+	image_string = pytesseract.image_to_string(image, lang='eng', config='--psm 1 --oem 3')
+	return image_string
+
+def grayscale(image):
+	return cv.cvtColor(image, cv.COLOR_BGR2GRAY)
+
+def img_preprocess(image):
+	image = cv.resize(image, (512,512))
+	cv.imwrite('convert.jpg', image)
+	im=cv.imread('convert.jpg')
+	im = im/255
+	im = np.expand_dims(im, axis=0)
+	return im
+
+def model_classify(image):
+	gray = grayscale(np.asarray(image))
+	image = img_preprocess(gray)
+	holistic_pred=model.predict(image)
+	sort_index=np.argsort(holistic_pred)[::-1]
+	df=pd.DataFrame({'Document_Type':doc_type,
+                         'Percentage':holistic_pred[0]})
+	df=df.sort_values('Percentage',ascending=False)
+	labels=df['Document_Type']
+	classification = df.iloc[0]['Document_Type']
+	percentage =  df.iloc[0]['Percentage']*100
+	df['Percentage'] = df['Percentage']*100
+	rank = df.iloc[0:5].reset_index(drop=True).set_index('Document_Type')['Percentage'].to_dict()
+
+	if percentage < THRESHOLD and percentage != "other":
+		try:
+			image_string = img_to_string(gray)
+			key_classification = key_classify(image_string)
+		except Exception as e:
+			f = open("log.txt", "a")
+			f.write(str(e))
+			f.write(traceback.format_exc())
+			f.close()
+		return {'classification':key_classification, 'accuracy': percentage, 'rank': rank} 
+	else:
+		return {'classification':classification, 'accuracy': percentage, 'rank': rank}
+
+def key_classify(string):
+	string = string.lower()
+	if "packing declaration" in string:
+		print("pkd check done")
+		return "pkd"
+	elif "packing list" in string:
+		print("pkl check done")
+		return "pkl"
+	elif "bill of lading" in string:
+		print("hbl check done")
+		return "hbl"
+	elif "invoice" in string:
+		print("civ check done: ")
+		return "civ"
+	else:
+		return "other" 
+
+def multipage_combine(predictions, file):
+	shared_type = {key:value['classification'] for key, value in predictions.items()}
+	res = {}
+	for i, v in shared_type.items():
+		res[v] = [i] if v not in res.keys() else res[v] + [i]
+	print(res)
+	merged_predictions = {}
+	inputpdf = PdfFileReader(file)
+	if inputpdf.isEncrypted:
+		try:
+			inputpdf.decrypt('')
+			print('File Decrypted (PyPDF2)')
+		except:
+			print("Decryption error")
+
+	for classification, pages in res.items():
+		average_accuracy = 0
+		output = PdfFileWriter()
+		for page in pages:
+			output.addPage(inputpdf.getPage(page-1))
+			average_accuracy += predictions[page]["accuracy"] 
+		split_file_name = file_name(file.filename)+"_"+classification+".pdf"
+		with open(data_folder+split_file_name, "wb") as outputStream:
+			output.write(outputStream)
+		average_accuracy = average_accuracy/len(pages)
+		merged_predictions[split_file_name] = {'classification':classification, 'pages':pages, 'accuracy':average_accuracy, 'path':os.path.abspath(data_folder+split_file_name)}
+
+	return merged_predictions
+
+def parse_classify(file):
+	"""
+	Receives file 
+	Note that this only reads the first sheet or page of the document,
+	since that's where the header/title is usually located. 
+
+	Returns classification of document based on keyword
+	"""
+
+	ext = file_ext(file.filename)
+	string = ""
+	accuracy = 0
+	rank = {}
+
+	if ext == "pdf":
+		images = convert_from_bytes(file.read()) #, grayscale=True
+		prediction = {}
+		for idx, image in enumerate(images):
+			prediction[idx+1]  = model_classify(image) # prediction should return page indexes to be merged and separated and classification
+		predictions = multipage_combine(prediction, file)
+
+	elif ext in ["jpg", "jpeg", "png"]:
+		pil_image = Image.open(file)
+		opencvImage = cv.cvtColor(np.array(pil_image), cv.COLOR_RGB2BGR)
+		predictions = model_classify(opencvImage)
+	elif ext == "docx":
+		doc = docx.Document(file)
+		fullText = []
+		for para in doc.paragraphs:
+			fullText.append(para.text)
+			string = '\n'.join(fullText)
+		classification = key_classify(string)
+		predictions = {'classification': classification}
+
+	elif ext in ["xls", "xlsx"]:
+		sheet = pd.read_excel(file, sheet_name=[0])
+		string = str(sheet)
+		classification = key_classify(string)
+		predictions = {'classification'}
+
+	return ext, predictions
+
+def update_model(X,y):
+	try:
+		gray = grayscale(X)
+		X = img_preprocess(gray)
+		le = preprocessing.LabelBinarizer()
+		le.fit(doc_type)
+		y = le.transform([y])
+		model.fit(X,y)
+		model.save('models/classify_256.h5', overwrite=True)
+		return "Success!"
+	except Exception as e:
+		#f = open("log.txt", "a")
+		#f.write(str(e))
+		#f.write(traceback.format_exc())
+		#f.close()
+		return e
+	#return "Success!"
